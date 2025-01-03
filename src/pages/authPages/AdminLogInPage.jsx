585398e521995b48d0478451307aaf6987f2fd5a
Mazin Emad <124470200+Mazin-emad@users.noreply.github.com>
2025-01-03 21:43:42 +0200

    fix .env 4

--- src/pages/authPages/AdminLogInPage.jsx
+++ src/pages/authPages/AdminLogInPage.jsx
@@ -5,7 +5,7 @@ import { useNavigate, Link } from "react-router-dom";
 import { saveTokenWithExpiration } from "../../context/localstorageAPI";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const ADMIN_LOGIN_ENDPOINT = `${API_BASE_URL}${
-  import.meta.env.VITE_ADMIN_LOGIN
+  import.meta.env.VITE_AUTH_LOGIN
 }`;
 
 const login = async ({ email, password }) => {
